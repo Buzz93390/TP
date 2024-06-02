e4433a1585f3e17d9b319bd118c95bb6600a044d
Buzz93390 <148759240+Buzz93390@users.noreply.github.com>
2024-06-02 19:00:31 +0200

    my_triggers.sql

--- my_triggers.sql
+++ my_triggers.sql
@@ -5,7 +5,7 @@ BEGIN
   INSERT INTO WORKERS (worker_id, firstname, lastname, age, start_date)
   VALUES (:NEW.worker_id, :NEW.firstname, :NEW.lastname, :NEW.age, :NEW.start_date);
 END;
-/
+
 
 CREATE OR REPLACE TRIGGER trg_no_update_delete_all_workers_elapsed
 BEFORE UPDATE OR DELETE ON ALL_WORKERS_ELAPSED
@@ -13,7 +13,7 @@ FOR EACH ROW
 BEGIN
   RAISE_APPLICATION_ERROR(-20001, 'Updates and deletions are not allowed on ALL_WORKERS_ELAPSED');
 END;
-/
+
 
 CREATE OR REPLACE TRIGGER trg_audit_robot
 AFTER INSERT ON ROBOTS
@@ -22,7 +22,7 @@ BEGIN
   INSERT INTO AUDIT_ROBOT (robot_id, date_added)
   VALUES (:NEW.robot_id, SYSDATE);
 END;
-/
+
 
 CREATE OR REPLACE TRIGGER trg_check_factories
 BEFORE INSERT OR UPDATE OR DELETE ON FACTORIES
@@ -38,7 +38,7 @@ BEGIN
     RAISE_APPLICATION_ERROR(-20002, 'Number of factories does not match the number of WORKERS_FACTORY_<N> tables');
   END IF;
 END;
-/
+
 
 CREATE OR REPLACE TRIGGER trg_worker_departure
 BEFORE UPDATE OF departure_date ON WORKERS
@@ -48,4 +48,4 @@ BEGIN
     :NEW.duration := :NEW.departure_date - :OLD.start_date;
   END IF;
 END;
-/
+
